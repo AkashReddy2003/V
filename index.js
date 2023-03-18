fb337389b5692cd44b191e6dad7a16d7854e02e2
unknown <akepatiakashreddy@gmail.com>
2023-03-18 22:36:16 +0530

    v

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser=require("body-parser");
+const app = express();
+const cors=require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://v:v@cluster0.bvria8p.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+    client.connect();
+    const collection=client.db().collection("controls");
+ 
+
+app.post("/sendControls",async(req,res)=>{
+    const lat=req.body.lat;
+    const lon=req.body.lon;
+    const direc=req.body.direc;
+    const angle=req.body.angle;
+
+    try{
+        await collection.insertOne({ 
+            lat:lat,
+            lon:lon,
+            direc:direc,
+            angle:angle,
+
+     });
+        res.send("Successfully Registerd");
+    }catch(e){
+        console.log(e);
+        res.send("Error");
+    }
+    
+})
+
+
+app.get("/getControls",async(req,res)=>{
+    const x=await collection.find().toArray();
+    res.send(x);
+})
+
+
+app.listen(process.env.PORT||4000,()=>{
+    console.log("Server Running");
+
+})
